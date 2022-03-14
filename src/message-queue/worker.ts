a52dd9f751bf5e669abd97178ced2040ffa4eaa3
Remah Amr <remah.alarabi@hamza.solutions>
2022-03-14 21:33:15 +0200

    handle consumer

---
+++ src/message-queue/worker.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { MessageQueueModule } from './test1-consumer.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(MessageQueueModule);
+  app.init();
+}
+
+bootstrap();
