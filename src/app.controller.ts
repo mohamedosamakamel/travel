c7bb1ca4b4561dc7137e7ad7459193ca204a197e
Remah Amr <remah.alarabi@hamza.solutions>
2023-05-02 17:30:57 +0300

    add kubernetes configuration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,26 +2,19 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Public } from './auth/decorators/public.decorator';
 import { MessageQueueService } from './message-queue/message-queue.service';
-import * as test from 'qiniu-js'
-import * as test2 from 'multer-s3'
-import * as test3 from 'multer-minio-storage'
-import * as test4 from 'minimist'
-import * as test5 from 'kafkajs'
-import * as test6 from 'graphql'
+import * as os from 'os';
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    // private readonly messageQueueService: MessageQueueService,
-  ) {}
+  ) // private readonly messageQueueService: MessageQueueService,
+  {}
 
-  // @Public()
-  // @Get()
-  // getHello() {
-  //   const duration = 30000
-  //   const start = Date.now();
-  //   while (Date.now() - start < duration) {}
-  // }
+  @Public()
+  @Get()
+  getHello() {
+    return { host: os.hostname() };
+  }
 
   // @Public()
   // @Get('publisher')
