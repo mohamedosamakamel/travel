437da49fb2a2bafb31ca480a7005165d74f737ca
Remah Amr <remah3335@gmail.com>
2023-06-10 18:31:25 -0400

    handle gracefully shutdown

--- dockerfiles/dev.dockerfile
+++ dockerfiles/dev.dockerfile
@@ -10,4 +10,14 @@ RUN npm install
 
 COPY . .
 
+RUN wget -O /usr/local/bin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v1.2.5/dumb-init_1.2.5_x86_64
+RUN chmod +x /usr/local/bin/dumb-init
+
 RUN npm run build
+
+ENV TINI_VERSION v0.19.0
+ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /tini
+RUN chmod +x /tini
+RUN apk add --no-cache tini
+ENTRYPOINT ["/sbin/tini", "--"]
+CMD ["node", "dist/main"]
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,16 +3,29 @@ import { AppService } from './app.service';
 import { Public } from './auth/decorators/public.decorator';
 import { MessageQueueService } from './message-queue/message-queue.service';
 import * as os from 'os';
+import { UserRepository } from 'src/users/users.repository';
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService, // private readonly messageQueueService: MessageQueueService,
-  ) {}
+    private readonly UserRepository: UserRepository
+  ) { }
 
   @Public()
   @Get()
-  getHello() {
+  async getHello() {
+    await this.wait(10000)
+    let test = await this.UserRepository.findOne({})
+    console.log(test)
     return { host: os.hostname() };
+  }
+
+  wait(ms) {
+    let start = new Date().getTime();
+    let end = start;
+    while (end < start + ms) {
+      end = new Date().getTime();
+    }
   }
 
   @Public()
